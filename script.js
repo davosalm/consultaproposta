79bc6822d73f76137d2ec584ddd2851725d1f241
davosalm <davosalm@gmail.com>
2024-05-07 18:31:23 -0300

    v0.1 de novo

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-// Função para carregar as proposições do deputado selecionado
-function carregarProposicoes() {
-    var deputadoId = $("#selectDeputados").val();
-    var deputadoNome = $("#selectDeputados option:selected").text();
-    var deputadoPartido = $("#selectDeputados option:selected").data("partido");
-    
-    if (deputadoId) {
+$(document).ready(function(){
+    // URL base da API
+    var baseUrl = "https://dadosabertos.camara.leg.br/api/v2/";
+
+    // Função para carregar os partidos
+    function carregarPartidos() {
+        $.get(baseUrl + "partidos", function(data){
+            var partidos = data.dados;
+            var selectPartidos = $("#selectPartidos");
+            selectPartidos.empty();
+            $.each(partidos, function(index, partido){
+                selectPartidos.append("<option value='" + partido.sigla + "'>" + partido.nome + " (" + partido.sigla + ")</option>");
+            });
+        });
+    }
+
+    // Função para carregar os deputados do partido selecionado
+    function carregarDeputados() {
+        var partidoSigla = $("#selectPartidos").val();
+        $.get(baseUrl + "deputados", {siglaPartido: partidoSigla}, function(data){
+            var deputados = data.dados;
+            var selectDeputados = $("#selectDeputados");
+            selectDeputados.empty();
+            $.each(deputados, function(index, deputado){
+                selectDeputados.append("<option value='" + deputado.id + "'>" + deputado.nome + "</option>");
+            });
+            selectDeputados.prop("disabled", false);
+        });
+    }
+
+    // Função para carregar as proposições do deputado selecionado
+    function carregarProposicoes() {
+        var deputadoId = $("#selectDeputados").val();
         $.get(baseUrl + "proposicoes", {idDeputadoAutor: deputadoId}, function(data){
             var proposicoes = data.dados;
             var listaProposicoes = $("#listaProposicoes");
@@ -12,12 +38,19 @@ function carregarProposicoes() {
             $.each(proposicoes, function(index, proposicao){
                 listaProposicoes.append("<li>" + proposicao.siglaTipo + " " + proposicao.numero + " - " + proposicao.ementa + "</li>");
             });
-
-            // Exibir nome e partido do deputado no topo da página
-            var deputadoInfo = $("#deputadoInfo");
-            deputadoInfo.html("<p class='deputado-name'>" + deputadoNome.toUpperCase() + "</p><p class='deputado-party'>" + deputadoPartido.toUpperCase() + "</p>");
         });
-    } else {
-        alert("Selecione um deputado antes de consultar as proposições.");
     }
-}
+
+    // Carregar os partidos ao carregar a página
+    carregarPartidos();
+
+    // Botão de consulta
+    $("#btnConsultar").click(function(){
+        carregarProposicoes();
+    });
+
+    // Carregar os deputados ao selecionar um partido
+    $("#selectPartidos").change(function(){
+        carregarDeputados();
+    });
+});
