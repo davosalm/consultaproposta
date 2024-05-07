1959e81b5ecdd34d1074b6dc5f96b7e0e6ee55e9
davosalm <davosalm@gmail.com>
2024-05-07 08:46:43 -0300

    Versão 0.1

---
+++ script.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    // URL base da API
+    var baseUrl = "https://dadosabertos.camara.leg.br/api/v2/";
+
+    // Função para carregar os partidos
+    function carregarPartidos() {
+        $.get(baseUrl + "partidos", function(data){
+            var partidos = data.dados;
+            var selectPartidos = $("#selectPartidos");
+            selectPartidos.empty();
+            selectPartidos.append("<option value='' disabled selected>Selecione um partido</option>");
+            $.each(partidos, function(index, partido){
+                selectPartidos.append("<option value='" + partido.sigla + "'>" + partido.nome + " (" + partido.sigla + ")</option>");
+            });
+        });
+    }
+
+    // Função para carregar os deputados
+    function carregarDeputados() {
+        $.get(baseUrl + "deputados", function(data){
+            var deputados = data.dados;
+            var selectDeputados = $("#selectDeputados");
+            selectDeputados.empty();
+            selectDeputados.append("<option value='' disabled selected>Selecione um deputado</option>");
+            $.each(deputados, function(index, deputado){
+                selectDeputados.append("<option value='" + deputado.id + "'>" + deputado.nome + "</option>");
+            });
+            selectDeputados.prop("disabled", false);
+        });
+    }
+
+    // Função para carregar as proposições do deputado selecionado
+    function carregarProposicoes() {
+        var deputadoId = $("#selectDeputados").val();
+        $.get(baseUrl + "proposicoes", {idDeputadoAutor: deputadoId}, function(data){
+            var proposicoes = data.dados;
+            var listaProposicoes = $("#listaProposicoes");
+            listaProposicoes.empty();
+            $.each(proposicoes, function(index, proposicao){
+                listaProposicoes.append("<li>" + proposicao.siglaTipo + " " + proposicao.numero + " - " + proposicao.ementa + "</li>");
+            });
+        });
+    }
+
+    // Carregar os partidos ao carregar a página
+    carregarPartidos();
+
+    // Botão de consulta
+    $("#btnConsultar").click(function(){
+        carregarProposicoes();
+    });
+
+    // Carregar os deputados ao selecionar um partido
+    $("#selectPartidos").change(function(){
+        carregarDeputados();
+    });
+});
