b1e2caaa537f2a4312caaaa399f1be5729bbd795
davosalm <davosalm@gmail.com>
2024-05-08 16:56:17 -0300

    v1.0

--- script.js
+++ script.js
@@ -1,58 +1,36 @@
 $(document).ready(function(){
-    // URL base da API
+
+    // URL puxada da API
     var baseUrl = "https://dadosabertos.camara.leg.br/api/v2/";
 
-    // Função para carregar os deputados
+    // Função que serve pra carregar os deputados/as
     function carregarDeputados() {
         $.get(baseUrl + "deputados", function(data){
             var deputados = data.dados;
             var selectDeputados = $("#selectDeputados");
             selectDeputados.empty();
             $.each(deputados, function(index, deputado){
-                // Verifica se a URL da imagem está disponível
-                if (deputado.urlFoto) {
-                    // Cria um novo objeto Image
-                    var img = new Image();
-                    // Define o atributo src com a URL da imagem
-                    img.src = deputado.urlFoto;
-                    // Adiciona um evento onload para garantir que a imagem foi carregada corretamente
-                    img.onload = function() {
-                        // Adiciona o deputado ao dropdown após a imagem ser carregada
-                        selectDeputados.append("<option value='" + deputado.id + "' data-partido='" + deputado.siglaPartido + "' data-nome='" + deputado.nome + "' data-uf='" + deputado.siglaUf + "' data-foto='" + img.src + "'>" + deputado.nome + "</option>");
-                    };
-                    // Adiciona um evento onerror para lidar com erros de carregamento da imagem
-                    img.onerror = function() {
-                        console.error("Erro ao carregar imagem para o deputado: " + deputado.nome);
-                    };
-                } else {
-                    // Se a URL da imagem não estiver disponível, adicione apenas o nome do deputado ao dropdown
-                    selectDeputados.append("<option value='" + deputado.id + "' data-partido='" + deputado.siglaPartido + "' data-nome='" + deputado.nome + "' data-uf='" + deputado.siglaUf + "'>" + deputado.nome + "</option>");
-                }
+                selectDeputados.append("<option value='" + deputado.id + "' data-partido='" + deputado.siglaPartido + "' data-nome='" + deputado.nome + "' data-uf='" + deputado.siglaUf + "'>" + deputado.nome + "</option>");
             });
             selectDeputados.prop("disabled", false);
         })
         .fail(function() {
-            console.log("Erro ao carregar os deputados.");
+            console.log("Erro ao carregar os deputados. Pode ser algum problema no banco da Câmara dos Deputados. Aguarde alguns segundos e tente novamente.");
         });
     }
 
-    // Função para carregar informações do deputado selecionado
+    // Carregar informações do deputado/a selecionado
     function carregarInfoDeputado() {
         var deputadoSelecionado = $("#selectDeputados option:selected");
         var nomeDeputado = deputadoSelecionado.attr("data-nome");
         var partidoDeputado = deputadoSelecionado.attr("data-partido");
         var ufDeputado = deputadoSelecionado.attr("data-uf");
-
         $(".deputado-name").text(nomeDeputado);
         $(".deputado-party").text("Partido: " + partidoDeputado);
         $(".deputado-state").text("UF: " + ufDeputado);
-
-        var fotoDeputado = deputadoSelecionado.attr("data-foto");
-        $(".deputado-foto").attr("src", fotoDeputado); // Atualiza a imagem do deputado
-        $(".deputado-info-container").show(); // Exibe as informações do deputado
+        $("#deputadoInfoContainer").show();
     }
-
-    // Função para carregar as proposições do deputado selecionado
+    // Isso daqui serve pra carregar as proposições do deputado selecionado, puxado através da API
     function carregarProposicoes() {
         var deputadoId = $("#selectDeputados").val();
 
@@ -65,14 +43,14 @@ $(document).ready(function(){
             });
         });
     }
-
     // Carregar os deputados ao carregar a página
     carregarDeputados();
-
     // Ao clicar em consultar, carregar informações do deputado e proposições
     $("#btnConsultar").click(function(){
         carregarInfoDeputado();
         carregarProposicoes();
-        $(".header-text").hide(); // Esconde o texto do cabeçalho
+        $(".header-text").hide();
+        var fotoDeputado = $("#selectDeputados option:selected").attr("data-foto");
+        $(".deputado-foto").attr("src", fotoDeputado);
     });
 });
